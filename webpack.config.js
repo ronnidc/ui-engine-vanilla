3070a2a892e11e447d51c454c3760d19ed405d5d
ronnidc (RDC) <ronnidc@gmail.com>
2020-02-10 23:45:58 +0100

    webpack: initial webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
+
+var distributionDir = 'dist';
+var sourceDir = 'src';
+
+module.exports = {
+	mode: 'development',
+  entry: {
+    app: `./${sourceDir}/index.js`,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+      contentBase: path.join(__dirname, distributionDir),
+      hot: true,
+      quiet: false, // Terminal console
+      noInfo: false, // Terminal console
+      //clientLogLevel: 'silent', // Browser console  
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, distributionDir),
+  },
+  plugins: [
+    new ManifestPlugin({
+      fileName: 'webpack-manifest.json',
+      basePath: `/${sourceDir}/`,
+      publicPath: `/${distributionDir}/`,
+      seed: {
+        name: 'Webpack Manifest'
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      favicon: `${sourceDir}/favicon.ico`,
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  }
+};
